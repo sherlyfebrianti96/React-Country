1dbffa7dcb4416c74a41a0a9f59cf4026b69fcb6
Sherly Febrianti <sherlyfebrianti96@yahoo.co.id>
2021-12-19 03:50:51 +0100

    Use MUI Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import CountriesPage from "./pages/Countries";
@@ -6,16 +7,19 @@ import LanguagesPage from "./pages/Languages";
 
 export const App: React.FunctionComponent = () => {
   const queryClient = new QueryClient();
+  const theme = createTheme();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/countries" element={<CountriesPage />} />
-          <Route path="/languages" element={<LanguagesPage />} />
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/countries" element={<CountriesPage />} />
+            <Route path="/languages" element={<LanguagesPage />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 };
